edcdf5431e6fdeb88e734e95d6199218e99598a3
kolyadom03 <110120292+kolyadom03@users.noreply.github.com>
2022-08-09 17:23:23 +0300

    Update Program.cs

--- Dz2/Program.cs
+++ Dz2/Program.cs
@@ -1,8 +1,1 @@
-﻿Console.Write("Введите целое число");
-int numberA = Convert.ToInt32(Console.ReadLine());
-int numberB = 2;
-while(numberB <= numberA)
-{
-        Console.Write( numberB + " ");
-        numberB = numberB = 2;
-}       
+
