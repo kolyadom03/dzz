3f21c6e7e8b49c7ee28b44e6793d9cb6d1e71db5
kolya <kolyadomashnikov2003@gmail.com>
2022-08-09 17:01:49 +0300

    first comit

---
+++ Dz2/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите целое число");
+int numberA = Convert.ToInt32(Console.ReadLine());
+int numberB = 2;
+while(numberB <= numberA)
+{
+        Console.Write( numberB + " ");
+        numberB = numberB = 2;
+}       
